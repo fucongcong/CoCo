5314822e1be6a1b9e7d4d8a926e169141627fa6a
fucongcong <fucongcong@clothesmake.com>
2019-07-17 17:16:04 +0800

    rm module

--- cm-user/cm-user-service/src/main/java/com/clothesmake/user/api/UserProfileService.java
+++
@@ -1,7 +0,0 @@
-package com.clothesmake.user.api;
-
-import com.clothesmake.user.dto.UserProfileDto;
-
-public interface UserProfileService {
-    UserProfileDto getUserProfile(int id);
-}
--- cm-user/cm-user-service/src/main/java/com/clothesmake/user/api/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.clothesmake.user.api;
-
-import com.clothesmake.user.dto.UserDto;
-
-public interface UserService {
-    UserDto getUser(int id);
-}
